30d59d3c7f95def2cc0cb63495eeec004719d454
niganth <niganth21@gmail.com>
2024-05-14 22:18:10 +0530

    second

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ const path = require('path')
 const dotenv = require('dotenv');
 dotenv.config({path:path.join(__dirname,"config/config.env")});
 
+const cors = require("cors");
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000",
+    "https://vadivel-mandy-2-0.vercel.app",
+
+  );
+
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE,PATCH, OPIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 
 app.use(express.json());
 app.use(cookieParser());
